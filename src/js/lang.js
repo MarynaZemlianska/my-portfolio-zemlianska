92873fba5ff66f972bb971fe56c03f65c8f35d91
Maryna Zemlianska <zemaruv@google.com>
2026-05-30 04:48:04 +0300

    Create english version of home page

--- src/js/lang.js
+++ src/js/lang.js
@@ -1,5 +1,15 @@
+function getLangFromPath(path) {
+    if (path.startsWith('/en')) return 'en';
+    return 'ua';
+}
+
+const currentLang = getLangFromPath(window.location.pathname);
+
 document.querySelectorAll('.lang-btn').forEach(btn => {
+    btn.classList.toggle('active', btn.dataset.langBtn === currentLang);
+
     btn.addEventListener('click', () => {
         window.location.href = btn.dataset.url;
     });
 });
+
